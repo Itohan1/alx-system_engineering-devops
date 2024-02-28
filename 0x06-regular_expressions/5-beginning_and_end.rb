b69dfc97c5973c4d513c55ec1bf0cae806a4c183
Itohan1 <momoduitohan5@gmail.com>
2024-02-28 04:20:56 -0800

    Not quite HBTN yet

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,5 +1,2 @@
 #!/usr/bin/env ruby
-"""The regular expression must be exactly matching a string that starts with h ends with n"""
-
-
-puts AGRV[0].scan(/h\wn/).join
+puts ARGV[0].scan(/^h.n$/).join
