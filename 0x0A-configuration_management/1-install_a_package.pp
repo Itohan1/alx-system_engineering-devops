1278cbca62d68db6604755be2ace241da977d766
Itohan <momoduitohan5@gmail.com>
2024-04-22 09:59:42 +0100

    install flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,5 +3,5 @@
 exec { 'flask':
   command => '/usr/bin/pip3 install flask',
   path    => ['/usr/bin', '/usr/local/bin'],
-  unless  => '/usr/bin/pip3 show flask'
+  unless  => '/usr/bin/pip3 show flask',
 }
